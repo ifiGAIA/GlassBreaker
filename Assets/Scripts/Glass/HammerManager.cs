1ea52021ccacded330b68063c7d68b8ad3f85457
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2021-11-08 14:34:18 +0800

    20211108_1

---
+++ Assets/Scripts/Glass/HammerManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class HammerManager : MonoBehaviour
+{
+    private Interactable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+        Debug.Log("4");
+    }
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+        Debug.Log("5");
+    }
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabTypes = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        if(interactable.attachedToHand == null && grabTypes != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabTypes);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+            Debug.Log("1");
+        }
+        else if(isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+            Debug.Log("2");
+        }
+         Debug.Log("3");
+    }
+}
