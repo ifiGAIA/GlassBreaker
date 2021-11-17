cbdce69fe7713948059cb1f8bb5bfdea3f218fb9
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2021-11-17 15:05:23 +0800

    20211117_1

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    Image image;
+    Color imagecolor;
+    public AudioClip knock;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        imagecolor = image.color;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Left_hammer" || other.gameObject.tag == "Right_hammer")
+        {
+            imagecolor.a = 0.8f;
+            image.color = new Color(image.color.r,image.color.g,image.color.b,imagecolor.a);
+            audioSource.PlayOneShot(knock);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Left_hammer" || other.gameObject.tag == "Right_hammer")
+        {
+            imagecolor.a = 0.4f;
+            image.color = new Color(image.color.r,image.color.g,image.color.b,imagecolor.a);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/Timecounting.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timecounting : MonoBehaviour
+{
+    float timer_f = 60f;
+    int timer_i = 0;
+    int minute = 0;
+    int second = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        timer_f -= Time.deltaTime;
+        timer_i = (int)timer_f;
+        turn_time();
+        Second();
+        //印出時間動作
+        // if (minute < 10) 
+        // {
+            
+        //     if (second < 10)
+        //     {
+        //         GetComponent<Text>().text = "秒數:0" + minute + ":0" + second;
+        //     }
+        //     else 
+        //     {
+        //         GetComponent<Text>().text = "秒數:0" + minute + ":" + second;
+        //     }
+        // }
+        // else 
+        // {
+        //     if (second < 10)
+        //     {
+        //         GetComponent<Text>().text = "秒數:" + minute + ":0" + second;
+        //     }
+        //     else
+        //     {
+        //         GetComponent<Text>().text = "秒數:" + minute + ":" + second;
+        //     }
+        // }
+    }
+    void Second()
+    {
+        if (second < 10 && second > 0)
+        {
+            GetComponent<Text>().text = "秒數 : 0" + second;
+        }
+        else if(second >=10)
+        {
+            GetComponent<Text>().text = "秒數 : " + second;
+        }
+        else
+        {
+            GetComponent<Text>().text = "秒數 : 00";
+        }
+
+    }
+    void turn_time() 
+    {
+        minute = timer_i / 60;
+        second = timer_i %60;
+    }
+}
